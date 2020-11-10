cca4d3f46eff17dcff72f4465506faf607b302d3
Alex Dang <alexdang1993374@gmail.com>
2020-11-10 17:47:21 +0700

    fetched data

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const express = require("express")
-const app = express()
-app.use(express.static("./"))
-app.listen(process.env.PORT || 3000,() => {
-    console.log("listening at port 3000")
-})
+const express = require("express");
+const app = express();
+app.use(express.static("./"));
+const axios = require("axios");
+
+let data;
+
+axios({
+  method: "GET",
+  url:
+    "https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/community",
+  headers: {
+    Authorization: `Bearer 11|lNYfuYmccaqcTsup2VZhgHavQ85FAX8OofXKah4u`,
+  },
+}).then((res) => {data = res.data; console.log(data)});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening at port 3000");
+});
+
+module.exports = data

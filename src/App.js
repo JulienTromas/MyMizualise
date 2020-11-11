33625a6a15f4107b9385b0fe4d28eb37cfdf3cd9
Alex Dang <alexdang1993374@gmail.com>
2020-11-11 07:38:07 +0700

    can make requests from the front end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const axios = require("axios");
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url:
+        "https://cors-anywhere.herokuapp.com/https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/community",
+      headers: {
+        Authorization: `Bearer 11|lNYfuYmccaqcTsup2VZhgHavQ85FAX8OofXKah4u`,
+      },
+    }).then((res) => {
+      setUsers(res.data);
+      console.log(users);
+    });
+  }, [users]);
+
   return (
     <div className="App">
       <header className="App-header">

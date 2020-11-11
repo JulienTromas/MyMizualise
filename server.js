11a07e8f6a176b98aa0855fc164173b3dd78b6af
Alex Dang <alexdang1993374@gmail.com>
2020-11-11 07:38:07 +0700

    can make requests from the front end

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 app.use(express.static("./"));
 const axios = require("axios");
+const csv = require("csv-parser");
+const fs = require("fs")
 
 let data;
+let users = [];
+let litters = 0
 
 axios({
   method: "GET",
@@ -14,8 +18,34 @@ axios({
   },
 }).then((res) => {data = res.data; console.log(data)});
 
+app.get("/api/users", (res, req) => {
+  axios({
+    method: "GET",
+    url:
+      "https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/community",
+    headers: {
+      Authorization: `Bearer 11|lNYfuYmccaqcTsup2VZhgHavQ85FAX8OofXKah4u`,
+    },
+  }).then((json) => res.send(json.data));
+})
+
+async function getUsers() {
+
+  return await fs.createReadStream("./refills-2.csv")
+        .pipe(csv())
+        .on("data", (row) => {
+          litters += +row["500"];
+        })
+        .on("end", () => {
+          console.log(litters);
+        });
+}
+
+getUsers()
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("listening at port 3000");
 });
 
-module.exports = data
+module.exports = {data, users}

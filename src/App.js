c90aa5201340446abadf230aed8f035794ed26f0
Julien Tromas <julien.tromas@gmail.com>
2020-11-12 11:09:46 +0900

    Working on the Bottles pages

--- src/App.js
+++ src/App.js
@@ -1,85 +1,91 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Bottles from './components/Bottles';
-import CarbonDioxyde from './components/CarbonDioxyde';
-import './App.css';
-import axios from 'axios';
-import Background from './img/pexels-daria-shevtsova-1680140.jpg';
+import {BrowserRouter, Route, Switch, NavLink} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Bottles from "./components/Bottles";
+import CarbonDioxyde from "./components/CarbonDioxyde";
+import "./App.css";
+import axios from "axios";
+import Background from "./img/pexels-daria-shevtsova-1680140.jpg";
 
 function App() {
-
   const [data, setData] = useState([]);
-  const [currentView, setCurrentView] = useState('Homepage');
-  const [scroll, setScroll] = useState(0)
+  const [currentView, setCurrentView] = useState("Homepage");
+  const [scroll, setScroll] = useState(0);
 
-  async function getData(){
+  async function getData() {
     await axios({
       method: "GET",
-      url: "/api/refills"
-    }).then(res => {
-      setData(res.data)
-      console.log(data.refill_amount)
+      url: "/api/refills",
+    }).then((res) => {
+      setData(res.data);
+      console.log(data.refill_amount);
     });
-  }  
+  }
 
   return (
-    <>
-      <div className="App">
-        <BrowserRouter>
-          <div className="header" style={{ backgroundImage: `url(${Background})`}}>
+    <BrowserRouter>
+      {currentView === "Homepage" ? (
+        <div className="App">
+          <div className="header" style={{backgroundImage: `url(${Background})`}}>
             <h1 className="headerText">mymizualize</h1>
-            <p className="headerNote">A guide to visualizing mymizu's impact on the environment</p>
+            <p className="headerNote">
+              A guide to visualizing mymizu's impact on the environment
+            </p>
           </div>
           <div>
             <button onClick={getData}>Show me the Money</button>
             <div>
-            <h1>{data.refill_amount}</h1>
+              <h1>{data.refill_amount}</h1>
             </div>
             <NavLink to="/Bottles">
-              <button onClick={() => {
-            props.setCurrentView("Bottles");
-          }}>
-              My bottles
+              <button
+                onClick={() => {
+                  setCurrentView("Bottles");
+                }}
+              >
+                My bottles
               </button>
             </NavLink>
             <NavLink to="/Co2">
-              <button onClick={() => {
-            props.setCurrentView("CarbonDioxyde");
-          }}>
-              My CO2
+              <button
+                onClick={() => {
+                  setCurrentView("CarbonDioxyde");
+                }}
+              >
+                My CO2
               </button>
             </NavLink>
           </div>
-          <div>
-          <Navbar />
-            <Switch>
-              <Route path="/Bottles" render={() => <Bottles data={data} scroll={scroll} setScroll={setScroll}/>} />
-              <Route path="/CarbonDioxyde" render={() => <CarbonDioxyde data={data} scroll={scroll} setScroll={setScroll}/>} />
-            </Switch>
-          </div> 
-          </BrowserRouter>
-          <div>
-            {/* Insert login area */}
-          </div>
-          </div>
-    </>
+        </div>
+      ) : (
+        <div>
+          <Navbar currentView={currentView} setCurrentView={setCurrentView} />
+        </div>
+      )}
+      <div>
+        <Switch>
+          <Route
+            path="/Bottles"
+            render={() => (
+              <Bottles
+                data={data}
+                scroll={scroll}
+                setScroll={setScroll}
+                setCurrentView={setCurrentView}
+              />
+            )}
+          />
+          <Route
+            path="/CarbonDioxyde"
+            render={() => (
+              <CarbonDioxyde data={data} scroll={scroll} setScroll={setScroll} />
+            )}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-// {currentView === "All Photos" ? (
-//   <div>
-//     <AllPhotos
-//       setCurrentView={setCurrentView}
-//       setSelectedPhoto={setSelectedPhoto}
-//       photos={photos}
-//       selectedPhoto={selectedPhoto}
-//     />
-//   </div>
-// ) : (
-//   <div>
-//     <SinglePhoto photos={photos} selectedPhoto={selectedPhoto} />
-//   </div>
-// )}
--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -1,48 +1,138 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 // import { ReactComponent as Bottle } from '../img/waterbottle.svg';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position'
+import {useScrollPosition} from "@n8tb1t/use-scroll-position";
+import Navbar from "./Navbar";
 
-export default function Bottles({scroll, setScroll, data}) {
-  const [yPosition, setYPosition] = useState(0)
-  
-    useScrollPosition(({ prevPos, currPos }) => {
-        setYPosition(-currPos.y)
-        console.log(yPosition);
-      })
+export default function Bottles({scroll, setScroll, data, setCurrentView}) {
+  const [yPosition, setYPosition] = useState(0);
+
+  useScrollPosition(({prevPos, currPos}) => {
+    setYPosition(-currPos.y);
+    console.log(yPosition);
+  });
+
+  useEffect(() => {
+    setCurrentView("Bottles");
+  }, []);
 
   return (
     <>
-    <div id="header"></div>
-    {yPosition >= 76 && yPosition < 250 ? (<div id="greenTopBanner" style={{backgroundColor:'green'}}>February</div>) :
-    yPosition >= 250 && yPosition < 427 ? (<div id="greenTopBanner" style={{backgroundColor:'red'}}>March</div>) :
-    yPosition >= 427 && yPosition < 600 ? (<div id="greenTopBanner" style={{backgroundColor:'orange'}}>April</div>) : 
-    yPosition >= 600 && yPosition < 778 ? (<div id="greenTopBanner" style={{backgroundColor:'yellow'}}>May</div>) :
-    yPosition >= 778 && yPosition < 954 ? (<div id="greenTopBanner" style={{backgroundColor:'orange'}}>June</div>) :
-    yPosition >= 954 && yPosition < 1131 ? (<div id="greenTopBanner" style={{backgroundColor:'purple'}}>July</div>) :
-    yPosition >= 1131 && yPosition < 1306 ? (<div id="greenTopBanner" style={{backgroundColor:'brown'}}>August</div>) :
-    yPosition >= 1306 && yPosition < 1482 ? (<div id="greenTopBanner" style={{backgroundColor:'aqua'}}>September</div>) :
-    yPosition >= 1482 && yPosition < 1658 ? (<div id="greenTopBanner" style={{backgroundColor:'chartreuse'}}>October</div>) :
-    yPosition >= 1658 ? (<div id="greenTopBanner" style={{backgroundColor:'yellow'}}>November</div>) :
-    (<div id="topBanner">January</div>)}
-    
-  <div id="textChanger"></div>
-    <div id="months">
-        <div>Number of plastic bottles saved by MyMizu users</div>
-        <div id="January" className="bottleMonth"><p>In January of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="February" className="bottleMonth"><p>In February of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="March" className="bottleMonth"><p>In March of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="April" className="bottleMonth"><p>In April of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="May" className="bottleMonth"><p>In May of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="June" className="bottleMonth"><p>In June of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="July" className="bottleMonth"><p>In July of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="August" className="bottleMonth"><p>In August of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="September" className="bottleMonth"><p>In September of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="October" className="bottleMonth"><p>In October of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-        <div id="November" className="bottleMonth"><p>In November of 2020, we prevented <em>1,892 plastic bottles</em> from being used</p></div>
-    </div>
+      <div id="header">
+        <Navbar />
+      </div>
+      <div id="wrapper">
+        {yPosition >= 76 && yPosition < 250 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "green"}}>
+            February
+          </div>
+        ) : yPosition >= 250 && yPosition < 427 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "red"}}>
+            March
+          </div>
+        ) : yPosition >= 427 && yPosition < 600 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "orange"}}>
+            April
+          </div>
+        ) : yPosition >= 600 && yPosition < 778 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "yellow"}}>
+            May
+          </div>
+        ) : yPosition >= 778 && yPosition < 954 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "orange"}}>
+            June
+          </div>
+        ) : yPosition >= 954 && yPosition < 1131 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "purple"}}>
+            July
+          </div>
+        ) : yPosition >= 1131 && yPosition < 1306 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "brown"}}>
+            August
+          </div>
+        ) : yPosition >= 1306 && yPosition < 1482 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "aqua"}}>
+            September
+          </div>
+        ) : yPosition >= 1482 && yPosition < 1658 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "chartreuse"}}>
+            October
+          </div>
+        ) : yPosition >= 1658 ? (
+          <div id="greenTopBanner" style={{backgroundColor: "yellow"}}>
+            November
+          </div>
+        ) : (
+          <div id="topBanner">January</div>
+        )}
+
+        <div id="whiteTransparentBlock"></div>
+        <div id="months">
+          <div>Number of plastic bottles saved by MyMizu users</div>
+          <div id="January" className="bottleMonth">
+            <p>
+              In January of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="February" className="bottleMonth">
+            <p>
+              In February of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="March" className="bottleMonth">
+            <p>
+              In March of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="April" className="bottleMonth">
+            <p>
+              In April of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="May" className="bottleMonth">
+            <p>
+              In May of 2020, we prevented <em>1,892 plastic bottles</em> from being used
+            </p>
+          </div>
+          <div id="June" className="bottleMonth">
+            <p>
+              In June of 2020, we prevented <em>1,892 plastic bottles</em> from being used
+            </p>
+          </div>
+          <div id="July" className="bottleMonth">
+            <p>
+              In July of 2020, we prevented <em>1,892 plastic bottles</em> from being used
+            </p>
+          </div>
+          <div id="August" className="bottleMonth">
+            <p>
+              In August of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="September" className="bottleMonth">
+            <p>
+              In September of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="October" className="bottleMonth">
+            <p>
+              In October of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+          <div id="November" className="bottleMonth">
+            <p>
+              In November of 2020, we prevented <em>1,892 plastic bottles</em> from being
+              used
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
-
-
-
